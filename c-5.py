c37b62908daad5c287e96200bb823676435fd736
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-09-24 22:47:34 +0900

    c-5完了

---
+++ c-5.py
@@ -0,0 +1,36 @@
+class Customer:
+    def __init__(self, first_name, family_name, age):
+        self.name = first_name + " " + family_name
+        self.age = age
+
+        if age <= 3:
+            self.money = "無料"
+        elif age < 20:
+            self.money = 1000
+        elif 20 <= age < 65:
+            self.money = 1500
+        elif 65 <= age:
+            self.money = 1200
+
+    def full_name(self):
+        return self.name
+
+    def entry_fee(self):
+        return self.money
+
+
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+ken.entry_fee()  # 1000 という値を返す
+print(ken.entry_fee())
+
+tom = Customer(first_name="Tom", family_name="Ford", age=57)
+tom.entry_fee()  # 1500 という値を返す
+print(tom.entry_fee())
+
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+ieyasu.entry_fee()  # 1200 という値を返す
+print(ieyasu.entry_fee())
+
+souta = Customer(first_name="souta", family_name="sakuragi", age=3)
+ieyasu.entry_fee()  # 無料 という値を返す
+print(souta.entry_fee())
