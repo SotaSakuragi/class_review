855370406ba19c5a5ee619dcb1a1087d7f83e420
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-09-24 22:19:02 +0900

    c-3完了

--- custmor.py
+++ custmor.py
@@ -3,8 +3,18 @@ class Customer:
         self.name = first_name + " " + family_name
         self.age = age
 
+        if age < 20:
+            self.money = 1000
+        elif 20 <= age < 65:
+            self.money = 1500
+        elif 65 <= age:
+            self.money = 1200
+
     def full_name(self):
         return self.name
+
+    def entry_fee(self):
+        return self.money
 
 
 """
@@ -16,7 +26,7 @@ tom = Customer(first_name="Tom", family_name="Ford")
 tom.full_name()  # "Tom Ford" という値を返す
 print(tom.full_name())
 """
-
+"""
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
 ken.age  # 15 という値を返す
 print(ken.age)
@@ -28,3 +38,17 @@ print(tom.age)
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
 ieyasu.age  # 73 という値を返す
 print(ieyasu.age)
+"""
+
+
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+ken.entry_fee()  # 1000 という値を返す
+print(ken.entry_fee())
+
+tom = Customer(first_name="Tom", family_name="Ford", age=57)
+tom.entry_fee()  # 1500 という値を返す
+print(tom.entry_fee())
+
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+ieyasu.entry_fee()  # 1200 という値を返す
+print(ieyasu.entry_fee())
