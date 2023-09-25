4e4ba56f501202b361356bc06da1f2fd42c64233
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-09-25 17:54:14 +0900

    c-2変更

--- custmor.py
+++ custmor.py
@@ -1,7 +1,8 @@
 class Customer:
-    def __init__(self, first_name, family_name):
+    def __init__(self, first_name, family_name, age):
         self.first_name = first_name
         self.family_name = family_name
+        self.age = age
 
     def full_name(self):
         return self.first_name + " " + self.family_name
@@ -22,7 +23,7 @@ tom = Customer(first_name="Tom", family_name="Ford")
 tom.full_name()  # "Tom Ford" という値を返す
 print(tom.full_name())
 """
-"""
+
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
 ken.age  # 15 という値を返す
 print(ken.age)
@@ -34,7 +35,7 @@ print(tom.age)
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
 ieyasu.age  # 73 という値を返す
 print(ieyasu.age)
-"""
+
 
 """
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
@@ -50,14 +51,14 @@ ieyasu.entry_fee()  # 1200 という値を返す
 print(ieyasu.entry_fee())
 """
 
-ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
-ken.info_csv()  # "Ken Tanaka,15,1000" という値を返す
-print(ken.info_csv())
+# ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+# ken.info_csv()  # "Ken Tanaka,15,1000" という値を返す
+# print(ken.info_csv())
 
-tom = Customer(first_name="Tom", family_name="Ford", age=57)
-tom.info_csv()  # "Tom Ford,57,1500" という値を返す
-print(tom.info_csv())
+# tom = Customer(first_name="Tom", family_name="Ford", age=57)
+# tom.info_csv()  # "Tom Ford,57,1500" という値を返す
+# print(tom.info_csv())
 
-ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
-ieyasu.info_csv()  # "Ieyasu Tokugawa,73,1200" という値を返す
-print(ieyasu.info_csv())
+# ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+# ieyasu.info_csv()  # "Ieyasu Tokugawa,73,1200" という値を返す
+# print(ieyasu.info_csv())
