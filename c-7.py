04508f307e55aa2d3d4f06a41079dab73851c135
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-09-25 21:12:28 +0900

    c-7 メソッド名csvからtabに

--- c-7.py
+++ c-7.py
@@ -1,28 +1,37 @@
 class Customer:
     def __init__(self, first_name, family_name, age):
-        self.name = first_name + " " + family_name
+        self.first_name = first_name
+        self.family_name = family_name
         self.age = age
 
-        if age < 20:
-            self.money = 1000
-        elif 20 <= age < 65:
-            self.money = 1500
-        elif 65 <= age:
-            self.money = 1200
+    def full_name(self):
+        return self.first_name + " " + self.family_name
 
-    def info_csv(self):
-        self.csv = self.name + "  " + str(self.age) + "  " + str(self.money)
-        return self.csv
+    def entry_fee(self):
+        if self.age <= 3:
+            return "無料"
+        elif self.age < 20:
+            return 1000
+        elif 20 <= self.age < 65:
+            return 1500
+        elif 65 <= self.age < 75:
+            return 1200
+        else:
+            return 500
+
+    def info_tab(self):
+        csv_string = self.full_name() + "  " + str(self.age) + "  " + str(self.entry_fee())
+        return csv_string
 
 
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
-ken.info_csv()  # "Ken Tanaka  15  1000" という値を返す
-print(ken.info_csv())
+ken.info_tab()  # "Ken Tanaka  15  1000" という値を返す
+print(ken.info_tab())
 
 tom = Customer(first_name="Tom", family_name="Ford", age=57)
-tom.info_csv()  # "Tom Ford  57  1500" という値を返す
-print(tom.info_csv())
+tom.info_tab()  # "Tom Ford  57  1500" という値を返す
+print(tom.info_tab())
 
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
-ieyasu.info_csv()  # "Ieyasu Tokugawa  73  1200" という値を返す
-print(ieyasu.info_csv())
+ieyasu.info_tab()  # "Ieyasu Tokugawa  73  1200" という値を返す
+print(ieyasu.info_tab())
